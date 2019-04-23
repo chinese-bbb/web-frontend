b779de88e678313c2851627f3e903ebe389919dc
e-cloud <saintscott119@gmail.com>
2019-04-24 00:03:25 +0800

    feat: setup basic routes structure

--- src/router.ts
+++ src/router.ts
@@ -14,12 +14,71 @@ export default new Router({
       component: Home,
     },
     {
+      path: '/search',
+      name: 'search',
+      component: () => import('./views/SearchResult.vue'),
+    },
+    {
+      path: '/customer',
+      name: 'customer',
+      component: () => import('./views/customer/CustomerPortal.vue'),
+      children: [
+        {
+          path: 'profile',
+          name: 'profile',
+          component: () => import('./views/customer/UserInfo.vue'),
+        },
+        {
+          path: 'file-complaint',
+          name: 'fileComplaint',
+          component: () => import('./views/customer/FileComplaint.vue'),
+        },
+        {
+          path: 'current-complaints',
+          name: 'currentComplaints',
+          component: () => import('./views/customer/CurrentComplaints.vue'),
+        },
+        {
+          path: 'complaints-history',
+          name: 'complaintsHistory',
+          component: () => import('./views/customer/ComplaintsHistory.vue'),
+        },
+        {
+          path: 'settings',
+          name: 'settings',
+          component: () => import('./views/customer/UserSettings.vue'),
+        },
+      ],
+    },
+    {
+      path: '/merchant',
+      name: 'merchant',
+      component: () => import('./views/merchant/MerchantPortal.vue'),
+      children: [
+        {
+          path: 'dashboard',
+          name: 'dashboard',
+          component: () => import('./views/merchant/Dashboard.vue'),
+        },
+        {
+          path: 'complaint-details',
+          name: 'complaintDetails',
+          component: () => import('./views/merchant/ComplaintDetails.vue'),
+        },
+      ],
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () => import('./views/About.vue'),
+    },
+    {
+      path: '/feedback',
+      name: 'feedback',
+      component: () => import('./views/Feedback.vue'),
     },
   ],
 });
