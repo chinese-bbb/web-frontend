7987cce412d8841414eff6bc18f34a6fb260a2ac
e-cloud <saintscott119@gmail.com>
2019-05-02 23:13:17 +0800

    feat: a basic user profile page implementation (UI only)

---
+++ src/components/UserComplaintCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="user-complaint-card">
+    <div class="media d-flex">
+      <div class="thumb"></div>
+      <div class="content ml-3">
+        <h3 class="title">xxxxxxxxxxxxxxxxxxxxxxx</h3>
+        <p class="issue-time">投诉时间：xxxxxxxxx</p>
+        <p class="brief-summary">简要内容：xxxxxxxxxxxxxxxxxxxxxxxx</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component({})
+  export default class UserComplaintCard extends Vue {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+  .media {
+    background-color: #fafafa;
+  }
+
+  .thumb {
+    width: 120px;
+    height: 120px;
+    background-color: #CCCCCC;
+  }
+
+  .title {
+    //font-size: ;
+  }
+
+  .content {
+    text-align: left;
+  }
+</style>
--- src/side-effects/fa-icon-imports.ts
+++ src/side-effects/fa-icon-imports.ts
@@ -1,6 +1,7 @@
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBell, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faUser, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faUser);
 library.add(faBell);
+library.add(faUserCircle);
