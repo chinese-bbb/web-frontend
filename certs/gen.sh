ff8cb3d4c35d69d27eeb3ffa85d7636a68f68492
e-cloud <saintscott119@gmail.com>
2019-07-31 03:03:14 +0800

    chore(certs): improve cert generating logic

--- certs/gen.sh
+++ certs/gen.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-openssl genrsa -des3 -out rootca.key 4096
-openssl req -x509 -new -nodes -key rootca.key -sha256 -days 1024 -out rootca.pem
-openssl req -new -sha256 -nodes -out server.csr -newkey rsa:4096 -keyout server.key -config server.csr.cnf
-openssl x509 -req -in server.csr -CA rootca.pem -CAkey rootca.key -CAcreateserial -out server.crt -days 500 -sha256 -extfile v3.ext
+sslcmd=openssl
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    MINGW*)     sslcmd="winpty openssl";;
+esac
+
+$sslcmd genrsa -des3 -out rootca.key 4096
+$sslcmd req -x509 -new -nodes -key rootca.key -sha256 -days 1024 -out rootca.crt -config root.csr.cnf
+$sslcmd req -new -sha256 -nodes -out server.csr -newkey rsa:4096 -keyout server.key -config server.csr.cnf
+$sslcmd x509 -req -in server.csr -CA rootca.crt -CAkey rootca.key -CAcreateserial -out server.crt -days 500 -sha256 -extfile v3.ext
