cfff055f0c1ac2ee7c4d97bcbf61bdae92857153
e-cloud <saintscott119@gmail.com>
2019-08-15 02:23:39 +0800

    feat: support element-ui on-demand load

--- src/plugins/element.ts
+++ src/plugins/element.ts
@@ -1,4 +1,110 @@
-import Vue from 'vue';
-import Element from 'element-ui';
+import {
+  Alert,
+  Badge,
+  Button,
+  ButtonGroup,
+  Card,
+  Checkbox,
+  CheckboxButton,
+  CheckboxGroup,
+  Col,
+  Container,
+  DatePicker,
+  Dialog,
+  Divider,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  Form,
+  FormItem,
+  Icon,
+  Image,
+  InfiniteScroll,
+  Input,
+  InputNumber,
+  Link,
+  Loading,
+  Main,
+  Message,
+  MessageBox,
+  Pagination,
+  Progress,
+  Radio,
+  RadioButton,
+  RadioGroup,
+  Row,
+  Table,
+  TableColumn,
+  TabPane,
+  Tabs,
+  Tag,
+  TimePicker,
+  TimeSelect,
+  Upload,
+} from 'element-ui';
 
-Vue.use(Element);
+const components = [
+  Pagination,
+  Dialog,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Input,
+  InputNumber,
+  Radio,
+  RadioGroup,
+  RadioButton,
+  Checkbox,
+  CheckboxButton,
+  CheckboxGroup,
+  Button,
+  ButtonGroup,
+  Table,
+  TableColumn,
+  DatePicker,
+  TimeSelect,
+  TimePicker,
+  Form,
+  FormItem,
+  Tabs,
+  TabPane,
+  Alert,
+  Icon,
+  Row,
+  Col,
+  Upload,
+  Progress,
+  Badge,
+  Card,
+  Container,
+  Main,
+  Link,
+  Divider,
+  Image,
+  Tag,
+];
+
+function install(Vue: any, opts: any = {}) {
+  components.forEach(component => {
+    Vue.component(component.name, component);
+  });
+
+  Vue.use(InfiniteScroll);
+  Vue.use(Loading.directive);
+
+  Vue.prototype.$ELEMENT = {
+    size: opts.size || '',
+    zIndex: opts.zIndex || 2000,
+  };
+
+  Vue.prototype.$loading = Loading.service;
+  Vue.prototype.$msgbox = MessageBox;
+  Vue.prototype.$alert = MessageBox.alert;
+  Vue.prototype.$confirm = MessageBox.confirm;
+  Vue.prototype.$prompt = MessageBox.prompt;
+  Vue.prototype.$message = Message;
+}
+
+export default {
+  install,
+};
